3e64276bfb0ca5354186ab39d24f24cbd0f7ff02
Hanif437 <Putrahanif437@gmail.com>
2021-09-29 09:59:09 +0700

    membuat visibility pada video ke 4

---
+++ visibility.php
@@ -0,0 +1,92 @@
+<?php
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit;
+  protected $diskon = 0;
+  private $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+  {
+    $this->judul      = $judul;
+    $this->penulis    = $penulis;
+    $this->penerbit   = $penerbit;
+    $this->harga      = $harga;
+  }
+
+  public function getHarga()
+  {
+    return $this->harga - ($this->harga * $this->diskon / 100);
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+    return $str;
+  }
+}
+
+class Novel extends Produk
+{
+  public $jmlHalaman;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->jmlHalaman = $jmlHalaman;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+    return $str;
+  }
+}
+
+class Game extends Produk
+{
+  public $waktuMain;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->waktuMain = $waktuMain;
+  }
+
+  public function setDiskon($diskon)
+  {
+    $this->diskon = $diskon;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
+    return $str;
+  }
+}
+
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+$produk1 = new Novel("Seirei Gensouki", "Kitayama Yuri", "Shousetsuka ni Narou", 70000, 250, 0, "Novel");
+$produk2 = new Game("Sword Art Online : Alicization Rising Steel", "Reki Kawahara", "ASCII Media Works", 0, 0, 800, "Game");
+
+echo $produk1->getInfoProduk();
+echo "</br>";
+echo $produk2->getInfoProduk();
+echo "<hr>";
+
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
