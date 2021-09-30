79930b50a761302e9fb0e9adcbed5b95d6cf703c
Hanif437 <Putrahanif437@gmail.com>
2021-09-30 09:00:18 +0700

    membuat constant pada video ke 7

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+
+// define('NAMA', 'Hanif');
+
+// echo NAMA;
+// echo"<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+
+// class Coba {
+//     const NAMA = 'Hanif';
+// }
+
+// echo Coba::NAMA;
+
+// echo __FILE__;
+
+// function coba(){
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+class Coba{
+    public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
+?>
